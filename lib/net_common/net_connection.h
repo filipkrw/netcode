897db6447a6a8adb93966c3121fba5734a9162f5
filipkrw <filipkrw.dev@gmail.com>
2024-03-14 00:08:35 +0000

    net_connection.h

---
+++ lib/net_common/net_connection.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "net_common.h"
+#include "net_tsqueue.h"
+#include "net_message.h"
+
+namespace olc
+{
+    namespace net
+    {
+        template <typename T>
+        class connection : public std::enable_shared_from_this<connectio<T>>
+        {
+        public:
+            connection() {}
+            virtual ~connection() {}
+
+        public:
+            bool ConnectToServer();
+            bool Disconnect();
+            bool IsConnected() const;
+
+        public:
+            bool send(const message<T> &msg);
+
+        protected:
+            asio::ip::tcp::socket m_socket;
+            asio::io_context &m_asioContext;
+
+            tsqueue<message<T>> m_qMessagesOut;
+            tsqueue<owned_message<T>> &m_qMessagesIn; // should <T> be here?
+        };
+    };
+}
